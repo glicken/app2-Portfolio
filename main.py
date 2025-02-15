36e1a51a600f801117cbaefed30e12b069830273
Nicholas <glicken@gmail.com>
2025-02-15 07:23:55 -0800

    changed size of top 2 columns

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1,3])
 
 with col1:
-    st.image("images/nicky.png")
+    st.image("images/nicky.png", width=200)
 
 with col2:
     st.title("Nicholas Smith")
